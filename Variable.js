dba6de08d448fec51a0f92c92ff27251df454871
Chandan Mali <88288584+Chandanmali@users.noreply.github.com>
2023-09-19 08:42:27 +0530

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,13 @@
+const fixedValue = 1234;
+let newFormat = "chandan";
+var olderValue = "1334";
+let notDeclare;
+
+//new format of console all value in one table
+console.table([fixedValue, newFormat, olderValue, notDeclare]);
+
+//fixedValue = 5678;
+newFormat = "suraj";
+olderValue = "0000";
+
+console.table([fixedValue, newFormat, olderValue, notDeclare]);
