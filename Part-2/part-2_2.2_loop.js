20b75bf003f572cf4c5c7ff9a47054059dd620cc
Chandan Mali <88288584+Chandanmali@users.noreply.github.com>
2023-11-28 09:20:12 +0530

    Add files via upload

---
+++ Part-2/part-2_2.2_loop.js
@@ -0,0 +1,13 @@
+//while loop
+let myArray = ["saktiman", "hanuman", "batman"];
+let i = 0;
+while (i < myArray.length) {
+       console.log(`${myArray[i]}`);
+       i = i + 1;
+}
+
+
+//Output
+// saktiman
+// hanuman
+// batman
